69745e71e1fc95e293f3ec4824901288ff277392
Michael Chelli <michaelchelli00@gmail.com>
2023-11-12 10:23:08 +0100

    removed unwraps

--- evaluator/src/lib.rs
+++ evaluator/src/lib.rs
@@ -1,7 +1,7 @@
+use std::str::FromStr;
 use wasi_common::pipe::*;
 use wasi_common::WasiCtx;
 use wasmtime::*;
-use std::str::FromStr;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Limits {
@@ -22,38 +22,58 @@ pub enum SubRes {
     TLE,
     MLE,
     RTE,
+    MFO,
 }
 
-pub fn run_gen(module: Module, engine: Engine, test_id: u64) -> String {
+pub fn run_gen(module: Module, engine: Engine, test_id: u64) -> anyhow::Result<String> {
     let stdout = WritePipe::new_in_memory();
     let mut ctx = deterministic_wasi_ctx::build_wasi_ctx();
     ctx.set_stdout(Box::new(stdout.clone()));
-    ctx.push_arg(&test_id.to_string()).unwrap();
-    run_wasi(&module, &engine, ctx, None, StoreLimits::default()).unwrap();
-    let contents: Vec<u8> = stdout.try_into_inner().unwrap().into_inner();
-    String::from_utf8(contents).unwrap()
+    ctx.push_arg(&test_id.to_string())?;
+    run_wasi(&module, &engine, ctx, None, StoreLimits::default())??;
+    let contents: Vec<u8> = stdout
+        .try_into_inner()
+        .map_err(|e| anyhow::anyhow!("error getting contents of stdout pipe: {:?}", e))?
+        .into_inner();
+    Ok(String::from_utf8(contents)?)
 }
 
-pub fn run_sub(module: Module, engine: Engine, input: String, limits: Limits) -> Result<SubRes,Error> {
+pub fn run_sub(
+    module: Module,
+    engine: Engine,
+    input: String,
+    limits: Limits,
+) -> anyhow::Result<SubRes> {
     let stdin = ReadPipe::from(input.as_bytes());
     let stdout = WritePipe::new_in_memory();
     let ctx = deterministic_wasi_ctx::build_wasi_ctx();
     ctx.set_stdin(Box::new(stdin.clone()));
     ctx.set_stdout(Box::new(stdout.clone()));
-    let store_limits = StoreLimitsBuilder::new().trap_on_grow_failure(true).instances(1).memories(1).memory_size(limits.memory as usize).tables(1).table_elements(limits.memory>>4).build();
-    let result = run_wasi(&module, &engine, ctx, Some(limits.cpu), store_limits);
+    let store_limits = StoreLimitsBuilder::new()
+        .trap_on_grow_failure(true)
+        .instances(1)
+        .memories(1)
+        .memory_size(limits.memory as usize)
+        .tables(1)
+        .table_elements(limits.memory >> 4)
+        .build();
+    let result = run_wasi(&module, &engine, ctx, Some(limits.cpu), store_limits)?;
     match result {
         Ok(()) => {
-            let contents: Vec<u8> = stdout.try_into_inner().unwrap().into_inner();
-            Ok(SubRes::OK(String::from_utf8(contents).unwrap()))
-        },
+            if let Ok(inner) = stdout.try_into_inner() {
+                let contents: Vec<u8> = inner.into_inner();
+                Ok(SubRes::OK(String::from_utf8(contents).unwrap()))
+            } else {
+                Ok(SubRes::MFO) //TODO
+            }
+        }
         Err(e) => {
             if let Some(&t) = e.root_cause().downcast_ref::<Trap>() {
                 match t {
                     Trap::OutOfFuel => Ok(SubRes::TLE),
                     Trap::MemoryOutOfBounds => Ok(SubRes::MLE),
                     Trap::TableOutOfBounds => Ok(SubRes::MLE),
-                    _ => Ok(SubRes::RTE)
+                    _ => Ok(SubRes::RTE),
                 }
             } else {
                 Err(e)
@@ -62,16 +82,24 @@ pub fn run_sub(module: Module, engine: Engine, input: String, limits: Limits) ->
     }
 }
 
-pub fn run_eval(module: Module, engine: Engine, test_id: u64, input: String) -> String {
+pub fn run_eval(
+    module: Module,
+    engine: Engine,
+    test_id: u64,
+    input: String,
+) -> anyhow::Result<String> {
     let stdin = ReadPipe::from(input.as_bytes());
     let stdout = WritePipe::new_in_memory();
     let mut ctx = deterministic_wasi_ctx::build_wasi_ctx();
     ctx.set_stdin(Box::new(stdin.clone()));
     ctx.set_stdout(Box::new(stdout.clone()));
-    ctx.push_arg(&test_id.to_string()).unwrap();
-    run_wasi(&module, &engine, ctx, None, StoreLimits::default()).unwrap();
-    let contents: Vec<u8> = stdout.try_into_inner().unwrap().into_inner();
-    String::from_utf8(contents).unwrap()
+    ctx.push_arg(&test_id.to_string())?;
+    run_wasi(&module, &engine, ctx, None, StoreLimits::default())??;
+    let contents: Vec<u8> = stdout
+        .try_into_inner()
+        .map_err(|e| anyhow::anyhow!("error getting contents of stdout pipe: {:?}", e))?
+        .into_inner();
+    Ok(String::from_utf8(contents)?)
 }
 
 pub fn evaluate_on_test(
@@ -82,18 +110,19 @@ pub fn evaluate_on_test(
     submission_engine: Engine,
     limits: Limits,
     test_id: u64,
-) -> TestEval {
-    let tc = run_gen(gen_wasm,contest_engine.clone(),test_id);
-    let sub_res = run_sub(sub_wasm,submission_engine,tc,limits).unwrap();
-    match sub_res {
+) -> anyhow::Result<TestEval> {
+    let tc = run_gen(gen_wasm, contest_engine.clone(), test_id)?;
+    let sub_res = run_sub(sub_wasm, submission_engine, tc, limits)?;
+    Ok(match sub_res {
         SubRes::OK(out) => {
-            let score = f64::from_str(run_eval(eval_wasm,contest_engine,test_id,out).trim()).unwrap();
+            let score = f64::from_str(run_eval(eval_wasm, contest_engine, test_id, out)?.trim())?;
             TestEval::Score(score)
-        },
+        }
         SubRes::TLE => TestEval::TLE,
         SubRes::MLE => TestEval::MLE,
         SubRes::RTE => TestEval::RTE,
-    }
+        SubRes::MFO => TestEval::Score(0f64),
+    })
 }
 
 fn evaluate_on_testset(
@@ -104,8 +133,20 @@ fn evaluate_on_testset(
     submission_engine: Engine,
     limits: Limits,
     testset_length: u64,
-) -> Vec<TestEval> {
-    (0..testset_length).map(|x| evaluate_on_test(gen_wasm.clone(),sub_wasm.clone(),eval_wasm.clone(),contest_engine.clone(),submission_engine.clone(),limits,x)).collect()
+) -> Vec<anyhow::Result<TestEval>> {
+    (0..testset_length)
+        .map(|x| {
+            evaluate_on_test(
+                gen_wasm.clone(),
+                sub_wasm.clone(),
+                eval_wasm.clone(),
+                contest_engine.clone(),
+                submission_engine.clone(),
+                limits,
+                x,
+            )
+        })
+        .collect()
 }
 
 struct State {
@@ -113,66 +154,88 @@ struct State {
     wasi: WasiCtx,
 }
 
-fn run_wasi(module: &Module, engine: &Engine, wasi: WasiCtx, fuel: Option<u64>, limits: StoreLimits) -> Result<(),Error> {
-    let mut linker: Linker<State> = Linker::new(&engine);
-    wasmtime_wasi::add_to_linker(&mut linker, |state| &mut state.wasi).unwrap();
-    
-    let mut store = Store::new(&engine, State{limits,wasi});
+fn run_wasi(
+    module: &Module,
+    engine: &Engine,
+    wasi: WasiCtx,
+    fuel: Option<u64>,
+    limits: StoreLimits,
+) -> anyhow::Result<anyhow::Result<()>> {
+    let mut linker: Linker<State> = Linker::new(engine);
+    wasmtime_wasi::add_to_linker(&mut linker, |state| &mut state.wasi)?;
+
+    let mut store = Store::new(engine, State { limits, wasi });
     store.limiter(|state| &mut state.limits);
     if let Some(f) = fuel {
-        store.add_fuel(f).unwrap();
+        store.add_fuel(f)?;
     }
-    linker.module(&mut store, "", &module).unwrap();
-    linker
-        .get_default(&mut store, "")
-        .unwrap()
-        .typed::<(), ()>(&store)
-        .unwrap()
-        .call(&mut store, ())
+    linker.module(&mut store, "", module)?;
+    let result = linker
+        .get_default(&mut store, "")?
+        .typed::<(), ()>(&store)?
+        .call(&mut store, ());
+    Ok(result)
 }
 
-fn get_submission_engine() -> Engine {
+fn get_submission_engine() -> anyhow::Result<Engine> {
     let mut config = Config::new();
     config.consume_fuel(true);
-    Engine::new(&config).unwrap()
+    Engine::new(&config)
 }
-fn get_contest_engine() -> Engine {
+fn get_contest_engine() -> anyhow::Result<Engine> {
     let config = Config::new();
-    Engine::new(&config).unwrap()
+    Engine::new(&config)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn eval_sub(sub_file: &str) -> Vec<TestEval> {
-        let submission_engine = get_submission_engine();
-        let contest_engine = get_contest_engine();
-        let gen_module = Module::from_file(&contest_engine, "./test_wasm/gen.wasm").unwrap();
-        let eval_module = Module::from_file(&contest_engine, "./test_wasm/eval.wasm").unwrap();
-        let sub_module = Module::from_file(&submission_engine, sub_file).unwrap();
-        let limits = Limits{memory:2000000,cpu:100000};
-        evaluate_on_testset(gen_module,sub_module,eval_module,contest_engine,submission_engine,limits,16)
+    fn eval_sub(sub_file: &str) -> anyhow::Result<Vec<TestEval>> {
+        let submission_engine = get_submission_engine()?;
+        let contest_engine = get_contest_engine()?;
+        let gen_module = Module::from_file(&contest_engine, "./test_wasm/gen.wasm")?;
+        let eval_module = Module::from_file(&contest_engine, "./test_wasm/eval.wasm")?;
+        let sub_module = Module::from_file(&submission_engine, sub_file)?;
+        let limits = Limits {
+            memory: 2000000,
+            cpu: 100000,
+        };
+        let ev = evaluate_on_testset(
+            gen_module,
+            sub_module,
+            eval_module,
+            contest_engine,
+            submission_engine,
+            limits,
+            16,
+        );
+        Ok(ev.into_iter().map(|x| x.unwrap()).collect())
     }
 
     #[test]
     fn ac_sub() {
-        assert_eq!(vec![TestEval::Score(1f64);16],eval_sub("./test_wasm/sub_ac.wasm"));
+        let ans = eval_sub("./test_wasm/sub_ac.wasm").unwrap();
+        assert_eq!(vec![TestEval::Score(1f64); 16], ans);
     }
     #[test]
     fn wa_sub() {
-        assert_eq!(vec![TestEval::Score(0f64);16],eval_sub("./test_wasm/sub_wa.wasm"));
+        let ans = eval_sub("./test_wasm/sub_wa.wasm").unwrap();
+        assert_eq!(vec![TestEval::Score(0f64); 16], ans);
     }
     #[test]
     fn rte_sub() {
-        assert_eq!(vec![TestEval::RTE;16],eval_sub("./test_wasm/sub_rte.wasm"));
+        let ans = eval_sub("./test_wasm/sub_rte.wasm").unwrap();
+        assert_eq!(vec![TestEval::RTE; 16], ans);
     }
     #[test]
     fn tle_sub() {
-        assert_eq!(vec![TestEval::TLE;16],eval_sub("./test_wasm/sub_tle.wasm"));
+        let ans = eval_sub("./test_wasm/sub_tle.wasm").unwrap();
+        assert_eq!(vec![TestEval::TLE; 16], ans);
     }
     #[test]
     fn mle_sub() {
-        assert_eq!(vec![TestEval::MLE;16],eval_sub("./test_wasm/sub_mle.wasm"));
+        let ans = eval_sub("./test_wasm/sub_mle.wasm").unwrap();
+        assert_eq!(vec![TestEval::MLE; 16], ans);
     }
 }
