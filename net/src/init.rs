6dbc11b9c9cb7c2ccf105a12de112fa942ccfd2f
Michael Chelli <michaelchelli00@gmail.com>
2023-11-21 17:35:54 +0100

    aoeu

--- net/src/init.rs
+++ net/src/init.rs
@@ -6,9 +6,17 @@ use tokio::net::UdpSocket;
 use tokio::task::AbortHandle;
 
 struct InitState {
-    // hashmap with things in phase 1
-    a: HashMap<(PubSigKey, PeerAddr), (SecKexKey, AbortHandle)>,
-    // hashmap with things done
+    // this hashmap contains info about connecting peers
+    // things are in this hashmap because you want to connect to them
+    // and either:
+    // - they did not send you their PubKexKey yet
+    // - or they did not send you a correct KeepAlive message yet
+    // once both these are satitsfied, the connection is considered established,
+    // so you should abort sending your PubKexKey with the AbortHandle
+    initting: HashMap<(PubSigKey, PeerAddr), (SecKexKey, AbortHandle)>,
+    // This is needed to disregard excess messages that may come from a peer,
+    // however if the PubKexKey is different, it is considered a request for a new connection
+    done: HashSet<(PubSigKey, PubKexKey)>,
 }
 
 async fn handle_init() {
